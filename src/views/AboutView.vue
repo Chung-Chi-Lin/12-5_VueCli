024dfaa685626e8c40a7a6d4066f9bf197491b23
Chung-Chi-Lin <z0925955648@gmail.com>
2022-12-05 15:27:50 +0800

    vueCli update

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,4 +2,15 @@
   <div class="about">
     <h1>This is an about page</h1>
   </div>
+  <card-component></card-component>
 </template>
+
+<script>
+import CardComponent from '@/components/CardComponent.vue';
+
+export default {
+  components: {
+    CardComponent,
+  },
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,24 @@
+<!-- eslint-disable vuejs-accessibility/label-has-for -->
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <v-form @submit="onSubmit" v-slot="{ errors, values, validate }">
+      {{ errors }} {{ values }}
+    <div class="mb-3">
+      <label for="email" class="form-label">Email</label>
+      <v-field id="email" name="email" type="email" class="form-control" rules="email|required"
+      :class="{ 'is-invalid': errors['email'] }"
+      v-model="user.email"
+      placeholder="請輸入 Email"></v-field>
+      <error-message class="invalid-feedback" name="email"></error-message>
+    </div>
+
+    <button class="btn btn-primary" type="button" @click="validate">驗證</button>
+    <button class="btn btn-primary" type="submit">Submit</button>
+  </v-form>
+
   </div>
 </template>
 
@@ -13,6 +30,23 @@ export default {
   name: 'HomeView',
   components: {
     HelloWorld,
+  },
+  data() {
+    return {
+      user: {},
+    };
+  },
+  methods: {
+    onSubmit() {
+      console.log(this.user);
+    },
+    // isPhone(value) {
+    //   const phoneNumber = /^(09)[0-9]{8}$/
+    //   return phoneNumber.test(value) ? true : '需要正確的電話號碼'
+    // },
+  },
+  created() {
+    console.log(this);
   },
 };
 </script>
